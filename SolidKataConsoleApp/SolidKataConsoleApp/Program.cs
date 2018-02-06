1293718924bc2aead5cc314cbc6bbca9050523c4
Eduardo Lagunas <eduardolagunas@outlook.com>
2018-02-06 21:39:17 +0100

    add step 1

---
+++ SolidKataConsoleApp/SolidKataConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidKataConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+
+            user.Username = "myUserName";
+            user.Password = "mySecurePassword";
+            user.Email = "myemail@email.me";
+            user.Photo = "base64photo";
+
+            user.Register(user);
+
+            user.UploadPhoto(user.Photo);
+
+            user.SendEmail(user.Email);
+        }
+    }
+}
